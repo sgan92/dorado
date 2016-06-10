f49090a4d79c66494f6b50287a6a26a2ccbae588
Stephanie Gan <stephanie.cai.gan@gmail.com>
2016-06-10 10:03:25 -0400

    fixed show page modal problem

--- frontend/stores/comments.js
+++ frontend/stores/comments.js
@@ -18,11 +18,13 @@ CommentsStore.receiveOne = function(comment){
 };
 
 CommentsStore.receiveAll = function(comments){
-  _comments[comments[0].image_id] = [];
-  comments.map( function(comment){
-    _comments[comment.image_id] = _comments[comment.image_id] || [];
-    _comments[comment.image_id].push(comment);
-  });
+  if (comments.length !== 0 ){
+    _comments[comments[0].image_id] = [];
+    comments.map( function(comment){
+      _comments[comment.image_id] = _comments[comment.image_id] || [];
+      _comments[comment.image_id].push(comment);
+    });
+  }
 };
 
 CommentsStore.__onDispatch = function(payload){
