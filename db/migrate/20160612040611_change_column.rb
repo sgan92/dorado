152b02f3ac2f3349693f821aec5cec530ed96bc8
sgan92 <stephanie.cai.gan@gmail.com>
2016-06-12 22:39:51 -0400

    fix typo

--- db/migrate/20160612040611_change_column.rb
+++ db/migrate/20160612040611_change_column.rb
@@ -1,6 +1,6 @@
 class ChangeColumn < ActiveRecord::Migration
   def change
     remove_column :notifications, :type
-    add_column :notifications, :notif_type, :string, null:
+    add_column :notifications, :notif_type, :string, null: false
   end
 end
